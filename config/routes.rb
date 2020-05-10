dc06ee0e67de43865415e34992548f64a02f4ebf
TvBach <kubach10x@gmail.com>
2020-05-10 23:46:57 +0700

    Add new

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'home' => 'static_pages#home'
   get 'login' => 'static_pages#login'
-  get 'signup' => 'static_pages#signup'
+  get 'signup' => 'users#new'
   root 'static_pages#home'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
